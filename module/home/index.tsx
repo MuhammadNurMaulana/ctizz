321de6e64ab5a6e51d363e7aac4ba47cd8670e53
MuhammadNurMaulana <muhammadnrmaulana@gmail.com>
2024-03-07 19:40:12 +0700

    cek

--- module/home/index.tsx
+++ module/home/index.tsx
@@ -1,11 +1,9 @@
-import { auth } from "@/lib/auth/auth"
 import { Navbar } from "../navbar"
 import DiscountSlideHome from "./components/discount-slide"
 import ShopCategories from "./components/shop-by-categories"
 import BestSeller from "./components/best-seller"
 
-export default async function HomeComponents() {
-  const session = await auth()
+export default function HomeComponents() {
   return (
     <div className="w-full">
       <Navbar />
